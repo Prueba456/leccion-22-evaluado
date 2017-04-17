af9d0bfaafbfde7b5e314ebda9317deb253394f6
Betsi Loayza <betsi18_12@hotmail.com>
2017-04-17 18:29:29 -0500

    mostrar todas las imagenes

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,6 +7,15 @@ for(var i=0; i<array.length; i++){
   spans[i].addEventListener("click", function(){
 
     this.parentNode.style.display = "none";
-  
+
   });
+
 }
+var mostrar = document.getElementsByTagName("button")[2];
+mostrar.addEventListener("click", function(){
+
+  array[0].style.display = "inline-block";
+  array[1].style.display = "inline-block";
+  array[2].style.display = "inline-block";
+  array[3].style.display = "inline-block";
+});
