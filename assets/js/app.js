3a22491b957827fd1d8bf8e93ebd5cbd068cf35b
Betsi Loayza <betsi18_12@hotmail.com>
2017-04-18 10:50:43 -0500

    funcionalidad de botones

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,11 +11,29 @@ for(var i=0; i<array.length; i++){
   });
 
 }
+var origenId = document.getElementById("origen");
+var extincionId = document.getElementById("extincion");
 var mostrar = document.getElementsByTagName("button")[2];
 mostrar.addEventListener("click", function(){
-
   array[0].style.display = "inline-block";
   array[1].style.display = "inline-block";
   array[2].style.display = "inline-block";
   array[3].style.display = "inline-block";
+});
+
+var extincion = document.getElementsByTagName("button")[1];
+extincion.addEventListener("click", function(){
+ if(extincionId.style.display == "inline-block"){
+   extincionId.style.display = "none";
+ }else{
+   extincionId.style.display = "inline-block";
+ }
+});
+var origen = document.getElementsByTagName("button")[0];
+origen.addEventListener("click", function(){
+ if(origenId.style.display == "inline-block"){
+   origenId.style.display = "none";
+ }else{
+   origenId.style.display = "inline-block";
+ }
 });
